d2eb7d22822fa9acff52b5b5aa9d34f37bb863db
Francis <meng.francis@gmail.com>
2014-02-19 23:44:57 -0500

    edit working

--- static/js/mymeme.js
+++ static/js/mymeme.js
@@ -1,26 +1,39 @@
-
 var rh = rh || {};
 rh.rosememe = rh.rosememe || {};
 rh.rosememe.endpoints = rh.rosememe.endpoints || {};
 rh.rosememe.selectedId = -1;
+rh.rosememe.memes = {};
+
 
 // ----------------------- Endpoints methods -----------------------
 
-rh.rosememe.endpoints.insertRoseMeme = function(caption, url){
+rh.rosememe.endpoints.insertRoseMeme = function (caption, url) {
 	var postJson = {
 		'caption': caption,
 		'image_url': url
 	};
 	console.log(postJson);
+	if (rh.rosememe.selectedId > 0) {
+		postJson.id = rh.rosememe.selectedId;
+		var img= $('#' + rh.rosememe.selectedId).children('img');
+		var caption= $('#' + rh.rosememe.selectedId).children('p');
+		rh.rosememe.memes[rh.rosememe.selectedId] = postJson;
+		$(caption).html(postJson.caption);
+		$(img).attr('src', postJson.image_url+'?'+Math.random());
+	}
 	gapi.client.rosememe.meme.insert(postJson).execute(function (resp) {
 		console.log(resp);
 		if (!resp.code) {
-			rh.rosememe.print(resp);
+			if (rh.rosememe.selectedId < 0) {
+				rh.rosememe.print(resp);
+				rh.rosememe.memes[resp.id] = resp;
+			}
 		}
+		rh.rosememe.selectedId = -1;
 	});
 };
 
-rh.rosememe.endpoints.listRoseMeme = function(){
+rh.rosememe.endpoints.listRoseMeme = function () {
 	gapi.client.rosememe.meme.list({'order': '-last_touch_date_time'}).execute(
 		function (resp) {
 			if (!resp.code) {
@@ -29,23 +42,24 @@ rh.rosememe.endpoints.listRoseMeme = function(){
 				// Loop through in reverse order since the newest goes on top.
 				for (var i = resp.items.length - 1; i >= 0; i--) {
 					rh.rosememe.print(resp.items[i]);
+					rh.rosememe.memes[resp.items[i].id] = resp.items[i];
 				}
 				rh.rosememe.enableEditButton();
 			}
 		});
 };
 
-rh.rosememe.endpoints.deleteRoseMeme = function(memeId){
+rh.rosememe.endpoints.deleteRoseMeme = function (memeId) {
 	gapi.client.rosememe.meme.delete({'id': memeId}).execute(
-	function(resp){
-		if(!resp.code){
-			$('#' + memeId).slideUp();
-		}
-	});
+		function (resp) {
+			if (!resp.code) {
+				$('#' + memeId).remove();
+			}
+		});
 };
 
 // ----------------------- Enable methods -----------------------
-rh.rosememe.enableButtons = function(){
+rh.rosememe.enableButtons = function () {
 	$('#display-add-meme-modal').click(function () {
 		$('#myModalLabel').html('Add a meme');
 		$('#add-meme-button').html("Add Meme");
@@ -54,28 +68,30 @@ rh.rosememe.enableButtons = function(){
 		$('#add-meme-modal').modal('show');
 	});
 
-	$('#add-meme-button').click(function(){
+	$('#add-meme-button').click(function () {
 		var url = $('#imgUrl').val();
 		var caption = $('#caption').val();
-		rh.rosememe.endpoints.insertRoseMeme(caption,url);
+		rh.rosememe.endpoints.insertRoseMeme(caption, url);
 		$('#add-meme-modal').modal('hide');
 	});
 
-	$('.pin').click(function(){
+	$('.pin').click(function () {
 
 	});
 };
 
-rh.rosememe.enableEditButton = function(){
-	$('.edit-meme').click(function(){
+rh.rosememe.enableEditButton = function () {
+	$('.edit-meme').click(function () {
 		$('#myModalLabel').html('Edit a meme');
 		$('#add-meme-button').html("Edit Meme");
-		$('#caption').val('');
-		$('#imgUrl').val('');
+		rh.rosememe.selectedId = rh.rosememe.getMemeId($(this));
+		var selectedMeme = rh.rosememe.memes[rh.rosememe.selectedId];
+		$('#caption').val(selectedMeme.caption);
+		$('#imgUrl').val(selectedMeme.image_url);
 		$('#add-meme-modal').modal('show');
 	});
 
-	$('.delete-meme').click( function(){
+	$('.delete-meme').click(function () {
 		var id = rh.rosememe.getMemeId($(this));
 		rh.rosememe.endpoints.deleteRoseMeme(id);
 	});
@@ -83,7 +99,7 @@ rh.rosememe.enableEditButton = function(){
 
 
 // ----------------------- Other methods -----------------------
-rh.rosememe.getMemeId = function($rowButton){
+rh.rosememe.getMemeId = function ($rowButton) {
 	var memeId = 0;
 	var $parent = null;
 	var parentEls = $rowButton.parents();
@@ -97,9 +113,9 @@ rh.rosememe.getMemeId = function($rowButton){
 	return memeId;
 };
 
-rh.rosememe.init = function(apiRoot){
+rh.rosememe.init = function (apiRoot) {
 	var apisToLoad;
-	var callback = function() {
+	var callback = function () {
 		console.log("Loaded an api");
 		if (--apisToLoad == 0) {
 			rh.rosememe.enableButtons();
@@ -111,10 +127,11 @@ rh.rosememe.init = function(apiRoot){
 	gapi.client.load('rosememe', 'v1', callback, apiRoot);
 };
 
-rh.rosememe.print = function(meme){
-	$('#columns').append('<div class = "pin" id = "'+meme.id+'">' +
+rh.rosememe.print = function (meme) {
+	$('#columns').append('<div class = "pin" id = "' + meme.id + '">' +
 		'<div class = "pin-icon"><i class="fa fa-pencil-square-o edit-meme"></i>' +
 		'<span class = "delete-meme">X</span></div>' +
-		'<img src = "'+meme.image_url+'"/>' +
-		'<p>'+meme.caption+'</p></div>');
+		'<img src = "' + meme.image_url + '"/>' +
+		'<p>' + meme.caption + '</p></div>');
+	rh.rosememe.enableEditButton();
 };
